9b20391b68a6912b949264049ff9d042e09ada88
eriyknas <anastasia9cheshuina@yandex.ru>
2026-02-04 21:27:51 +0300

    vvod

--- backend/program.cs
+++ backend/program.cs
@@ -1,4 +1,24 @@
 ﻿
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Нас зовут Настя и Полина");
-Console.WriteLine(DateTime.Now);
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("Нас зовут Настя и Полина");
+// Console.WriteLine(DateTime.Now);
+
+
+Console.WriteLine("Введите ваше имя:");
+string name = Console.ReadLine();
+Console.WriteLine($"Привет, {name}!");
+
+Console.WriteLine($"Текущая дата и время: {DateTime.Now}");
+Console.WriteLine($"День недели: {DateTime.Now.DayOfWeek}");
+
+Console.WriteLine("Хотите продолжить? (yes/no)");
+string answer = Console.ReadLine();
+
+if (answer.ToLower() == "yes")
+{
+    Console.WriteLine("Продолжаем работу...");
+}
+else
+{
+    Console.WriteLine("Завершение работы.");
+}
